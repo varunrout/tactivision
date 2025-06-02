818dc836bb273e7b416f866255946fae3d57b484
Varun Rout <varun_rout@outlook.com>
2025-06-02 09:05:47 +0100

    Added more mock data

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -7,55 +7,268 @@ export const NAV_LINKS = [
 ];
 
 export const MOCK_COMPETITIONS = [
-  { id: "comp1", name: "Premier League" },
-  { id: "comp2", name: "La Liga" },
-  { id: "comp3", name: "Serie A" },
-  { id: "comp4", name: "Bundesliga" },
-  { id: "comp5", name: "Ligue 1" },
+  {
+    "id": "9",
+    "name": "1. Bundesliga"
+  },
+  {
+    "id": "43",
+    "name": "FIFA World Cup"
+  },
+  {
+    "id": "11",
+    "name": "La Liga"
+  },
+  {
+    "id": "7",
+    "name": "Ligue 1"
+  },
+  {
+    "id": "1",
+    "name": "Premier League"
+  },
+  {
+    "id": "3",
+    "name": "Serie A"
+  },
+  {
+    "id": "13",
+    "name": "UEFA Champions League"
+  },
+  {
+    "id": "55",
+    "name": "UEFA Euro"
+  },
+  {
+    "id": "53",
+    "name": "UEFA Women's Euro"
+  },
+  {
+    "id": "72",
+    "name": "Women's World Cup"
+  }
 ];
 
 export const MOCK_SEASONS = {
-  comp1: [
-    { id: "s1-2324", name: "2023/2024" },
-    { id: "s1-2223", name: "2022/2023" },
-  ],
-  comp2: [
-    { id: "s2-2324", name: "2023/2024" },
-  ],
-  // ... add more seasons for other competitions
+  "9": [
+    {
+      "id": "281",
+      "name": "2023/2024"
+    }
+  ]
 };
 
 export const MOCK_TEAMS = {
-  "s1-2324": [ // Teams for Premier League 2023/2024
-    { id: "team1", name: "Manchester City", logoUrl: "https://placehold.co/40x40.png?text=MC" },
-    { id: "team2", name: "Arsenal", logoUrl: "https://placehold.co/40x40.png?text=AR" },
-    { id: "team3", name: "Liverpool", logoUrl: "https://placehold.co/40x40.png?text=LP" },
-  ],
-   "s2-2324": [ // Teams for La Liga 2023/2024
-    { id: "team4", name: "Real Madrid", logoUrl: "https://placehold.co/40x40.png?text=RM" },
-    { id: "team5", name: "Barcelona", logoUrl: "https://placehold.co/40x40.png?text=BC" },
-  ],
-  // ... add more teams
+  "281": [
+    { "id": "904", "name": "Bayer Leverkusen", "logoUrl": "" },
+    { "id": "201", "name": "FC Heidenheim", "logoUrl": "" },
+    { "id": "868", "name": "Bochum", "logoUrl": "" },
+    { "id": "872", "name": "Darmstadt 98", "logoUrl": "" },
+    { "id": "169", "name": "Bayern Munich", "logoUrl": "" },
+    { "id": "167", "name": "Borussia Dortmund", "logoUrl": "" },
+    { "id": "170", "name": "Eintracht Frankfurt", "logoUrl": "" },
+    { "id": "171", "name": "Werder Bremen", "logoUrl": "" },
+    { "id": "172", "name": "VfL Wolfsburg", "logoUrl": "" },
+    { "id": "173", "name": "Bayer 04 Leverkusen II", "logoUrl": "" },
+    { "id": "174", "name": "1. FC Köln", "logoUrl": "" },
+    { "id": "175", "name": "RB Leipzig", "logoUrl": "" },
+    { "id": "176", "name": "1. FC Union Berlin", "logoUrl": "" },
+    { "id": "177", "name": "Hoffenheim", "logoUrl": "" },
+    { "id": "178", "name": "Schalke 04", "logoUrl": "" },
+    { "id": "179", "name": "Freiburg", "logoUrl": "" },
+    { "id": "180", "name": "Stuttgart", "logoUrl": "" },
+    { "id": "181", "name": "Augsburg", "logoUrl": "" }
+  ]
 };
 
 export const MOCK_MATCHES = {
-  team1: [ // Matches for Manchester City
-    { id: "match1", name: "Man City vs Arsenal - 2023-10-08", date: "2023-10-08" },
-    { id: "match2", name: "Man City vs Liverpool - 2023-11-25", date: "2023-11-25" },
+  "904": [
+    { "id": "3895302", "name": "Bayer Leverkusen vs Bayern Munich - 2024-04-14", "date": "2024-04-14" },
+    { "id": "3895294", "name": "Borussia Dortmund vs Bayer Leverkusen - 2024-04-13", "date": "2024-04-13" },
+    { "id": "3895283", "name": "Bayer Leverkusen vs Eintracht Frankfurt - 2024-04-07", "date": "2024-04-07" },
+    { "id": "3895268", "name": "Hoffenheim vs Bayer Leverkusen - 2024-03-30", "date": "2024-03-30" },
+    { "id": "3895256", "name": "Bayer Leverkusen vs Augsburg - 2024-03-16", "date": "2024-03-16" },
+    { "id": "3895239", "name": "Schalke 04 vs Bayer Leverkusen - 2024-03-10", "date": "2024-03-10" },
+    { "id": "3895226", "name": "Bayer Leverkusen vs Borussia Dortmund - 2024-03-02", "date": "2024-03-02" },
+    { "id": "3895209", "name": "VfL Wolfsburg vs Bayer Leverkusen - 2024-02-24", "date": "2024-02-24" },
+    { "id": "3895196", "name": "Bayer Leverkusen vs Werder Bremen - 2024-02-17", "date": "2024-02-17" },
+    { "id": "3895178", "name": "Eintracht Frankfurt vs Bayer Leverkusen - 2024-02-10", "date": "2024-02-10" },
+    { "id": "3895167", "name": "Bayer Leverkusen vs Hoffenheim - 2024-02-03", "date": "2024-02-03" },
+    { "id": "3895154", "name": "RB Leipzig vs Bayer Leverkusen - 2024-01-27", "date": "2024-01-27" },
+    { "id": "3895138", "name": "Bayer Leverkusen vs Borussia Mönchengladbach - 2024-01-20", "date": "2024-01-20" },
+    { "id": "3895119", "name": "Stuttgart vs Bayer Leverkusen - 2024-01-13", "date": "2024-01-13" },
+    { "id": "3895105", "name": "Bayer Leverkusen vs Freiburg - 2024-01-06", "date": "2024-01-06" },
+    { "id": "3895092", "name": "Augsburg vs Bayer Leverkusen - 2023-12-17", "date": "2023-12-17" },
+    { "id": "3895079", "name": "Bayer Leverkusen vs Hoffenheim - 2023-12-09", "date": "2023-12-09" },
+    { "id": "3895061", "name": "Eintracht Frankfurt vs Bayer Leverkusen - 2023-11-25", "date": "2023-11-25" },
+    { "id": "3895045", "name": "Bayer Leverkusen vs Werder Bremen - 2023-11-12", "date": "2023-11-12" },
+    { "id": "3895028", "name": "1. FC Union Berlin vs Bayer Leverkusen - 2023-11-05", "date": "2023-11-05" },
+    { "id": "3895002", "name": "Bayer Leverkusen vs Augsburg - 2023-10-29", "date": "2023-10-29" },
+    { "id": "3894984", "name": "Schalke 04 vs Bayer Leverkusen - 2023-10-21", "date": "2023-10-21" },
+    { "id": "3894966", "name": "Bayer Leverkusen vs Borussia Dortmund - 2023-10-14", "date": "2023-10-14" },
+    { "id": "3894948", "name": "VfL Wolfsburg vs Bayer Leverkusen - 2023-10-07", "date": "2023-10-07" },
+    { "id": "3894932", "name": "Bayer Leverkusen vs FC Heidenheim - 2023-09-30", "date": "2023-09-30" },
+    { "id": "3894919", "name": "Stuttgart vs Bayer Leverkusen - 2023-09-17", "date": "2023-09-17" },
+    { "id": "3894903", "name": "Bayern Munich vs Bayer Leverkusen - 2023-09-10", "date": "2023-09-10" },
+    { "id": "3894887", "name": "Bayer Leverkusen vs VfL Wolfsburg - 2023-09-03", "date": "2023-09-03" },
+    { "id": "3894870", "name": "Eintracht Frankfurt vs Bayer Leverkusen - 2023-08-26", "date": "2023-08-26" },
+    { "id": "3894852", "name": "Bayer Leverkusen vs Schalke 04 - 2023-08-19", "date": "2023-08-19" },
+    { "id": "3894835", "name": "Borussia Dortmund vs Bayer Leverkusen - 2023-08-12", "date": "2023-08-12" },
+    { "id": "3894813", "name": "Bayer Leverkusen vs 1. FC Union Berlin - 2023-08-05", "date": "2023-08-05" }
   ],
-  team2: [ // Matches for Arsenal
-    { id: "match3", name: "Arsenal vs Man Utd - 2023-09-03", date: "2023-09-03" },
+  "201": [
+    { "id": "3894932", "name": "Bayer Leverkusen vs FC Heidenheim - 2023-09-30", "date": "2023-09-30" },
+    { "id": "3895025", "name": "FC Heidenheim vs VfL Wolfsburg - 2023-11-11", "date": "2023-11-11" },
+    { "id": "3894940", "name": "Stuttgart vs FC Heidenheim - 2023-10-01", "date": "2023-10-01" }
+  ],
+  "868": [
+    { "id": "3895267", "name": "Eintracht Frankfurt vs Bochum - 2024-03-30", "date": "2024-03-30" },
+    { "id": "3894960", "name": "Bochum vs Hoffenheim - 2023-10-14", "date": "2023-10-14" }
+  ],
+  "872": [
+    { "id": "3894842", "name": "FC Heidenheim vs Darmstadt 98 - 2023-08-11", "date": "2023-08-11" },
+    { "id": "3895290", "name": "Darmstadt 98 vs Eintracht Frankfurt - 2024-04-11", "date": "2024-04-11" }
+  ],
+  "169": [
+    { "id": "3895302", "name": "Bayer Leverkusen vs Bayern Munich - 2024-04-14", "date": "2024-04-14" },
+    { "id": "3894903", "name": "Bayern Munich vs Bayer Leverkusen - 2023-09-10", "date": "2023-09-10" }
+  ],
+  "167": [
+    { "id": "3895294", "name": "Borussia Dortmund vs Bayer Leverkusen - 2024-04-13", "date": "2024-04-13" },
+    { "id": "3894966", "name": "Bayer Leverkusen vs Borussia Dortmund - 2023-10-14", "date": "2023-10-14" }
+  ],
+  "170": [
+    { "id": "3895283", "name": "Bayer Leverkusen vs Eintracht Frankfurt - 2024-04-07", "date": "2024-04-07" },
+    { "id": "3895167", "name": "Bayer Leverkusen vs Hoffenheim - 2024-02-03", "date": "2024-02-03" },
+    { "id": "3895178", "name": "Eintracht Frankfurt vs Bayer Leverkusen - 2024-02-10", "date": "2024-02-10" }
+  ],
+  "171": [
+    { "id": "3895196", "name": "Bayer Leverkusen vs Werder Bremen - 2024-02-17", "date": "2024-02-17" },
+    { "id": "3895045", "name": "Bayer Leverkusen vs Werder Bremen - 2023-11-12", "date": "2023-11-12" }
+  ],
+  "172": [
+    { "id": "3895209", "name": "VfL Wolfsburg vs Bayer Leverkusen - 2024-02-24", "date": "2024-02-24" },
+    { "id": "3894948", "name": "VfL Wolfsburg vs Bayer Leverkusen - 2023-10-07", "date": "2023-10-07" }
+  ],
+  "173": [],
+  "174": [
+    { "id": "3895138", "name": "Bayer Leverkusen vs Borussia Mönchengladbach - 2024-01-20", "date": "2024-01-20" }
+  ],
+  "175": [
+    { "id": "3895154", "name": "RB Leipzig vs Bayer Leverkusen - 2024-01-27", "date": "2024-01-27" }
+  ],
+  "176": [
+    { "id": "3895002", "name": "Bayer Leverkusen vs Augsburg - 2023-10-29", "date": "2023-10-29" },
+    { "id": "3894813", "name": "Bayer Leverkusen vs 1. FC Union Berlin - 2023-08-05", "date": "2023-08-05" }
+  ],
+  "177": [
+    { "id": "3895268", "name": "Hoffenheim vs Bayer Leverkusen - 2024-03-30", "date": "2024-03-30" },
+    { "id": "3895068", "name": "Hoffenheim vs Bayer Leverkusen - 2023-12-09", "date": "2023-12-09" },
+    { "id": "3894960", "name": "Bochum vs Hoffenheim - 2023-10-14", "date": "2023-10-14" }
+  ],
+  "178": [
+    { "id": "3895239", "name": "Schalke 04 vs Bayer Leverkusen - 2024-03-10", "date": "2024-03-10" },
+    { "id": "3895004", "name": "Schalke 04 vs Bayer Leverkusen - 2023-10-21", "date": "2023-10-21" }
+  ],
+  "179": [
+    { "id": "3895105", "name": "Bayer Leverkusen vs Freiburg - 2024-01-06", "date": "2024-01-06" },
+    { "id": "3894885", "name": "Freiburg vs Bayer Leverkusen - 2023-10-03", "date": "2023-10-03" }
+  ],
+  "180": [
+    { "id": "3895119", "name": "Stuttgart vs Bayer Leverkusen - 2024-01-13", "date": "2024-01-13" },
+    { "id": "3894919", "name": "Stuttgart vs Bayer Leverkusen - 2023-09-17", "date": "2023-09-17" }
+  ],
+  "181": [
+    { "id": "3895092", "name": "Augsburg vs Bayer Leverkusen - 2023-12-17", "date": "2023-12-17" },
+    { "id": "3894961", "name": "Augsburg vs Bayer Leverkusen - 2023-10-23", "date": "2023-10-23" }
   ]
-  // ... add more matches
 };
 
 export const MOCK_PLAYERS = {
-  team1: [ // Players for Manchester City
-    { id: "player1", name: "Erling Haaland", position: "Forward", photoUrl: "https://placehold.co/80x80.png?text=EH" , teamBadgeUrl: MOCK_TEAMS["s1-2324"][0].logoUrl},
-    { id: "player2", name: "Kevin De Bruyne", position: "Midfielder", photoUrl: "https://placehold.co/80x80.png?text=KB", teamBadgeUrl: MOCK_TEAMS["s1-2324"][0].logoUrl },
-  ],
-  team2: [ // Players for Arsenal
-    { id: "player3", name: "Bukayo Saka", position: "Forward", photoUrl: "https://placehold.co/80x80.png?text=BS", teamBadgeUrl: MOCK_TEAMS["s1-2324"][1].logoUrl },
+  "904": [
+    {
+      "id": "8667",
+      "name": "Lukáš Hrádecký",
+      "position": "Goalkeeper",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    },
+    {
+      "id": "27133",
+      "name": "Odilon Kossonou",
+      "position": "Right Center Back",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    },
+    {
+      "id": "8221",
+      "name": "Jonathan Tah",
+      "position": "Center Back",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    },
+    {
+      "id": "30606",
+      "name": "Edmond Fayçal Tapsoba",
+      "position": "Left Center Back",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    },
+    {
+      "id": "41411",
+      "name": "Nathan Tella",
+      "position": "Right Wing Back",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    },
+    {
+      "id": "26517",
+      "name": "Alejandro Grimaldo García",
+      "position": "Left Wing Back",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    },
+    {
+      "id": "28268",
+      "name": "Exequiel Alejandro Palacios",
+      "position": "Right Defensive Midfield",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    },
+    {
+      "id": "34995",
+      "name": "Florian Wirtz",
+      "position": "Attacking Midfield",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    },
+    {
+      "id": "30607",
+      "name": "Wout Weghorst",
+      "position": "Center Forward",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    },
+    {
+      "id": "3500",
+      "name": "Granit Xhaka",
+      "position": "Left Center Midfield",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    },
+    {
+      "id": "8804",
+      "name": "Jonas Hofmann",
+      "position": "Right Center Forward",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    },
+    {
+      "id": "32289",
+      "name": "Victor Okoh Boniface",
+      "position": "Left Center Forward",
+      "photoUrl": "",
+      "teamBadgeUrl": ""
+    }
   ]
-  // ... add more players
 };
